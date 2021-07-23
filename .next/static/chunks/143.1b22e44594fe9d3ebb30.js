9d9a26d34692a43cc501cfd1caf5d3ab6d1b5c12
T0jgli <kovalikadam00@gmail.com>
2021-07-23 23:18:43 +0200

    remove

--- .next/static/chunks/143.1b22e44594fe9d3ebb30.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[143],{8143:function(){}}]);
--- .next/static/chunks/984.5d2c99bc2a16a7467242.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[984],{5984:function(){}}]);
